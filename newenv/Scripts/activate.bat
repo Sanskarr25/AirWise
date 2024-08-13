39e2cf3d24a96f1a695447fb60e6acea9c0a460a
Sanskarr25 <sanskarshete225@gmail.com>
2024-08-14 00:30:59 +0530

    Added .bat file to the repository

--- newenv/Scripts/activate.bat
+++ newenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\Air Quality\AirQualityIndex-Prediction-master\newenv
+set VIRTUAL_ENV=D:\Projects\AirWise\newenv
 
 if not defined PROMPT set PROMPT=$P$G
 
