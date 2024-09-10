80f2348fd737dc386d6d2c5c70ada7f978b2f501
Sanskarr25 <sanskarshete225@gmail.com>
2024-09-11 01:56:56 +0530

    made sure website functional with the help of JavaScript

---
+++ static/js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-links a");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navLinks.classList.remove("active");
+}
